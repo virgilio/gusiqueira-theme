2ca3a84e4ee2d06edf5ae9ae77e4e8ec3e01f61b
Virgilio Santos <virgilio.santos@gmail.com>
2014-01-13 16:01:39 -0200

    adding media gallery javascript (on page-media file)

--- page-media.php
+++ page-media.php
@@ -9,23 +9,46 @@
 
 <script type="text/javascript">
     jQuery(document).ready(function(){
-        function rnd_snd() {
-            return (Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1);
-        }
-
-        function rnd(mean, stdev) {
-            console.log(mean + 'x' + stdev);
-            return Math.round(rnd_snd()*stdev+mean);
-        }
+        /*
+         *  normRand: returns normally distributed random numbers
+          */
+        function normRand() {
+              var x1, x2, rad;
+               
+               do {
+                   x1 = 2 * Math.random() - 1;
+                   x2 = 2 * Math.random() - 1;
+                   rad = x1 * x1 + x2 * x2;
+               } while(rad >= 1 || rad == 0);
+                                               
+               var c = Math.sqrt(-1 * Math.log(rad) / rad);
+               //return x1 * c;
+               // console.log(c % 2 - 1);
+               return c % 2 - 1;
+        };
         
+        function AscendingNumberSort (a,b) {return a-b;}
+        function DescendingNumberSort(a,b) {return b-a;}
+
         var cheight = jQuery("#content article").height();
         var cwidth = jQuery("#content").width();
-        var numimages = jQuery(".galley-item").length;
+        var numimages = jQuery(".gallery-item").size();
 
+        var aside = (cwidth - 994)/2;
         jQuery(".gallery-item").each(function(){
-            console.log(rnd(cwidth, cwidth/numimages));
-            jQuery(this).css({'top' : cheight*(Math.random()%cheight)});
-            jQuery(this).css({'left' : rnd(cwidth, cwidth/numimages)});
+            var l = Math.abs(normRand()) * cwidth;
+            var h = 0;
+            if(l > aside && l < cwidth - aside){
+                h = Math.random() * cheight/1.5;
+            } else {
+                h = Math.random() * cheight/3;     
+                var img = jQuery(this).find("img");               
+                if(l + img.width() > cwidth){
+                    l = l - img.width();
+                }
+            }
+            jQuery(this).css({'top' : h}); 
+            jQuery(this).css({'left' : l});
             jQuery(this).fadeIn(200);
         });
     });
