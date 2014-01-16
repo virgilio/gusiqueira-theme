53c91effeae5f7d360041abc6496a8836b0990c9
Ana Rute <anarutecvm@gmail.com>
2014-01-16 10:53:49 -0200

    video template completed

--- category-videos.php
+++ category-videos.php
@@ -1,24 +1,29 @@
+<?php wp_enqueue_script('jquery'); ?>
 <?php  get_header(); ?>
 
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/video.css">
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.leanModal.min.js"></script>    
 <script type="text/javascript">
-    
+    jQuery( document ).ready( function(){
+        jQuery("a[rel*=leanModal]").leanModal();
+    })
 </script>
 <div id="content">
-    <div id="videos">
+    <div id="videos" class="videos-container">
     <?php 
         $n = true;    
         $i = 0;
         while ( have_posts() ) : the_post();
             if($i % 2 == 0) {
     ?>
-        <div class="video-column">
+        <div class="video-column <?php echo $n ? "odd" : "even"; ?>">
     <?php   
             } 
     ?>
-        <div class="video <?php echo $n ? "odd" : "even"; ?>">
+        <div class="video">
             <div class="video-image">
-                <a href="#" id="video-link-<?php echo $post->ID; ?>">
+                <a href="#video-<?php echo $post->ID; ?>" rel="leanModal" 
+                    id="video-link-<?php echo $post->ID; ?>">
     <?php
             if ( has_post_thumbnail() ) {
                 the_post_thumbnail("medium");
@@ -26,7 +31,7 @@
     ?>
                 </a>
             </div>
-            <div class="video-post-content">
+            <div class="video-post-content" id="video-<?php echo $post->ID; ?>">
                 <?php the_content(); ?>
             </div>
         </div>
@@ -43,9 +48,6 @@
     </div>
     <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
     <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
-</div>
-
-<div class="modal">
 </div>
 
 <?php get_footer(); ?>
